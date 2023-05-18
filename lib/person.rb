f2b31d73247193f4e40baeb6c0ae3016b2a5b10d
laura-paullette <laura.paullette@student.moringaschool.com>
2023-05-18 11:23:41 +0300

    completed

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+groot = Person.new
+groot.talk
+groot.walk
+
